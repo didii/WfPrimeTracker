f96994acad6f116e283a87bf96fee8ad0d221673
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-04-19 17:35:40 +0200

    Imported scss and js the webpack way

--- WfPrimeTracker.Server/ClientApp/modules.ts
+++ WfPrimeTracker.Server/ClientApp/modules.ts
@@ -1,3 +1,10 @@
+// Libraries
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/js/solid';
+
+// Vue modules
 import Vue from 'vue';
 import VueLazyload from 'vue-lazyload';
 
