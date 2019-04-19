888afca4bc78175fb45f0de93a52b926e605b464
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-04-19 23:41:05 +0200

    Display none when filtered for performance

--- WfPrimeTracker.Server/ClientApp/components/PrimeItemsContainerView.vue
+++ WfPrimeTracker.Server/ClientApp/components/PrimeItemsContainerView.vue
@@ -1,18 +1,25 @@
 <template>
     <div data-component="primeitemscontainerview" class="items-container py-2">
-        <PrimeItemsOptions :globalOptions="saveData.globalOptions" class="options" />
-        <div class="row">
+        <PrimeItemsOptions
+            :globalOptions="saveData.globalOptions"
+            class="options"
+        />
+        <SummaryView :primeItems="primeItems" :saveData="saveData.primeItems" />
+        <div v-if="primeItems.length" class="row">
             <div
-                v-for="primeItem in filteredPrimeItems"
+                v-for="primeItem in primeItems"
                 :key="primeItem.id"
                 :id="primeItem.id"
-                class="col-lg-6 col-12 p-2"
+                :class="['col-lg-6', 'col-12', 'p-2', isFiltered(primeItem) ? 'd-none' : '']"
             >
                 <PrimeItemView
                     :primeItem="primeItem"
                     :saveData="primeItemSaveData(primeItem)"
                 />
             </div>
+        </div>
+        <div v-else class="nothing">
+            Nothing found!
         </div>
     </div>
 </template>
@@ -23,12 +30,15 @@ import { getModule } from 'vuex-module-decorators';
 import GlobalModule from '@/stores/GlobalModule';
 import { ISaveData, IPrimeItemSaveData } from '@/services/LoadService';
 import { PrimeItem } from '@/models/PrimeItem';
-import PrimeItemView from './PrimeItemView.vue';
 import PrimeItemsOptions from './PrimeItemsOptions.vue';
+import SummaryView from './SummaryView.vue';
+import PrimeItemView from './PrimeItemView.vue';
+import AOS from 'aos';
 
 @Component({
     components: {
         PrimeItemsOptions,
+        SummaryView,
         PrimeItemView,
     }
 })
@@ -37,6 +47,11 @@ export default class PrimeItemsContainerView extends Vue {
 
     @Prop({ type: Array, required: true }) public primeItems!: PrimeItem[];
     @Prop({ type: Object, required: true }) public saveData!: ISaveData;
+
+    public updated() {
+        // Force AOS to refresh so that display-none element are detected
+        AOS.refresh();
+    }
 
     private get filteredPrimeItems(): PrimeItem[] {
         let result = this.primeItems;
@@ -47,6 +62,16 @@ export default class PrimeItemsContainerView extends Vue {
             result = result.filter(i => new RegExp(this.globalModule.searchQuery, 'i').test(i.name));
         }
         return result;
+    }
+
+    private get isFiltered(): (primeItem: PrimeItem) => boolean {
+        return item => {
+            if (this.saveData.globalOptions.hideCompleted && this.saveData.primeItems[item.id].isChecked)
+                return true;
+            if (this.globalModule.searchQuery && !new RegExp(this.globalModule.searchQuery, 'i').test(item.name))
+                return true;
+            return false;
+        }
     }
 
     private get primeItemSaveData(): (primeItem: PrimeItem) => IPrimeItemSaveData {
@@ -60,5 +85,9 @@ export default class PrimeItemsContainerView extends Vue {
     position: fixed;
     right: 0.5rem;
     z-index: 5;
+}
+.nothing {
+    text-align: center;
+    padding: 1rem;
 }
 </style>
