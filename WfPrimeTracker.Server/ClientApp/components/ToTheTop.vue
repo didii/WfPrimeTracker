1e148ac9bea8e4c98ea933a7cb03bda258ee55c8
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-04-21 11:37:09 +0200

    Added scroll to top component

---
+++ WfPrimeTracker.Server/ClientApp/components/ToTheTop.vue
@@ -0,0 +1,16 @@
+<template>
+    <button data-component="tothetop" @click="toTheTop" class="btn badge badge-primary">
+        Top <i class="fas fa-arrow-up"></i>
+    </button>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ToTheTop extends Vue {
+    private toTheTop() {
+        window.scrollTo(0, 0);
+    }
+}
+</script>
