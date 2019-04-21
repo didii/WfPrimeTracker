05204e141a07df00fdd9cf030a3b5cb310d0f49f
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-04-21 16:58:50 +0200

    Configure router for about page

--- WfPrimeTracker.Server/ClientApp/router.ts
+++ WfPrimeTracker.Server/ClientApp/router.ts
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from './components/Home.vue';
-// import Mockup from './components/Mockup.vue';
+import Home from '@/components/Home.vue';
+import About from '@/components/About.vue';
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
     { path: '/', redirect: '/home' },
     { path: '/home', component: Home },
+    { path: '/about', component: About },
     { path: '*', redirect: '/home' },
 ];
 
