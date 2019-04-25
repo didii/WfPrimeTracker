385938cc2492d5eff83550b26fec62be93b00542
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-04-25 23:16:00 +0200

    Compiler warning

--- WfPrimeTracker.Business/Jobs/FullScraperJob.cs
+++ WfPrimeTracker.Business/Jobs/FullScraperJob.cs
@@ -5,11 +5,12 @@ using Hangfire.Server;
 namespace WfPrimeTracker.Business.Jobs {
     internal class FullScraperJob : IFullScraperJob {
         /// <inheritdoc />
-        public async Task Invoke(PerformContext context) {
+        public Task Invoke(PerformContext context) {
             var relicRewardsJob = BackgroundJob.ContinueWith<IRelicRewardsScraperJob>(context.BackgroundJob.Id, job => job.Invoke(null));
             var primeItemsJob =
                 BackgroundJob.ContinueWith<IPrimeItemsScraperJob>(relicRewardsJob, job => job.Invoke(null));
             var blueprintJob = BackgroundJob.ContinueWith<IBlueprintScraperJob>(primeItemsJob, job => job.Invoke(null));
+            return Task.CompletedTask;
         }
     }
 }
