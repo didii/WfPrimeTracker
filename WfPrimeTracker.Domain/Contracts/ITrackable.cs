7eb08b74762849e9fcc8ddeeae0aa0b30df54666
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-04-29 17:45:27 +0200

    Added ITrackable interface

---
+++ WfPrimeTracker.Domain/Contracts/ITrackable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WfPrimeTracker.Domain.Users;
+
+namespace WfPrimeTracker.Domain.Contracts {
+    public interface ITrackable {
+        DateTimeOffset CreatedOn { get; set; }
+        DateTimeOffset ModifiedOn { get; set; }
+    }
+}
