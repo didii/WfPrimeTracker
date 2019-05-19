ab3755d22d359e06f0a348b2cc9f3b4e8f2a3189
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-05-19 13:10:01 +0200

    Added the UserController

---
+++ WfPrimeTracker.Server/Controllers/V1/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WfPrimeTracker.Business.Services;
+using WfPrimeTracker.Dtos.UserData;
+using WfPrimeTracker.Infrastructure;
+using WfPrimeTracker.Server.Models;
+
+namespace WfPrimeTracker.Server.Controllers.V1 {
+    public class UsersController : ApiController {
+        private readonly IUserService _service;
+
+        public UsersController(IUserService service) {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<UserSaveDataDto> GetUserData() {
+            var result = await _service.GetUserData();
+            return result;
+        }
+
+        [HttpPost]
+        public async Task SaveUserData(UserSaveDataDto dto) {
+            await _service.SaveUserData(dto);
+        }
+    }
+}
