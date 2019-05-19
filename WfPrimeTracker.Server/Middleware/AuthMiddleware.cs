afc80e082f6bd1595b18664eb107c90ae927879b
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-05-19 13:12:10 +0200

    Created Auth middleware

---
+++ WfPrimeTracker.Server/Middleware/AuthMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using WfPrimeTracker.Infrastructure;
+using WfPrimeTracker.Server.Models;
+
+namespace WfPrimeTracker.Server.Middleware {
+    public class AuthMiddleware {
+        private readonly RequestDelegate _next;
+
+        public AuthMiddleware(RequestDelegate next) {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext, IUserData userData) {
+            var ip = httpContext.Connection.RemoteIpAddress;
+            if (httpContext.Request.Headers.TryGetValue("AnonymousId", out var anonIdValues) && anonIdValues.Count >= 1 && Guid.TryParse(anonIdValues[0], out var anonId)) {
+                ((UserData)userData).InitializeAnonymous(ip, anonId);
+            }
+
+            return _next(httpContext);
+        }
+    }
+}
---
+++ WfPrimeTracker.Server/Models/UserData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WfPrimeTracker.Infrastructure;
+
+namespace WfPrimeTracker.Server.Models {
+    public class UserData : IUserData, IDisposable {
+        private bool _isAnonymous;
+        private Guid _anomymousId;
+        private string _email;
+        private IPAddress _ip;
+
+        public bool IsInitialized { get; private set; } = false;
+
+        public bool IsAnonymous => Ensure(_isAnonymous);
+
+        public Guid AnonymousId => Ensure(_anomymousId);
+
+        public string Email => Ensure(_email);
+
+        public IPAddress Id => Ensure(_ip);
+
+        public void InitializeAnonymous(IPAddress ip, Guid anonymousId) {
+            _isAnonymous = true;
+            _anomymousId = anonymousId;
+            _email = null;
+            IsInitialized = true;
+        }
+
+        public void InitializeOauth(IPAddress ip, string email, Guid? guid = null) {
+            _isAnonymous = false;
+            _anomymousId = guid.HasValue ? guid.Value : Guid.Empty;
+            _email = email;
+            IsInitialized = true;
+        }
+
+        public void Dispose() {
+            IsInitialized = false;
+        }
+
+        private T Ensure<T>(T value) {
+            if (!IsInitialized)
+                throw new UserDataNotInitializedException();
+            return value;
+        }
+    }
+
+    public class UserDataNotInitializedException : Exception {
+        public UserDataNotInitializedException() : base("UserData was not initialized before use") {}
+    }
+}
