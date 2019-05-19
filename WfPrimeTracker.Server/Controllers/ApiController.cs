7cebe8591df3307aaa7318e27c1fdb677fbfed6d
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-05-19 13:09:10 +0200

    Base API controller to unify routes

---
+++ WfPrimeTracker.Server/Controllers/ApiController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WfPrimeTracker.Server.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : Controller {
+    }
+}
