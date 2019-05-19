e93e0021e41b76a62b14a9d794373f5af14f1fd9
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-05-19 13:14:38 +0200

    Base logic for anonymous login

--- WfPrimeTracker.Server/ClientApp/App.vue
+++ WfPrimeTracker.Server/ClientApp/App.vue
@@ -13,6 +13,7 @@ import { getModule } from 'vuex-module-decorators';
 import GlobalModule from '@/stores/GlobalModule';
 import { PrimeItem } from '@/models/PrimeItem';
 import NavMenu from '@/components/NavMenu.vue';
+import { Dictionary } from 'vuex';
 
 @Component({
     components: {
@@ -20,5 +21,54 @@ import NavMenu from '@/components/NavMenu.vue';
     }
 })
 export default class App extends Vue {
+    private globalModule = getModule(GlobalModule, this.$store);
+
+    public mounted() {
+        this.setAnonId(this.$route.query);
+        
+    }
+
+    @Watch('$route.query') private onQueryParamsChange(query: Dictionary<string | (string | null)[]>) {
+        this.setAnonId(query);
+    }
+
+    private setAnonId(queryParams: Dictionary<string | (string | null)[]>) {
+        // First use URL
+        if (!this.setAnonIdFromQueryParams(queryParams)) {
+            // If URL not available, check local storage
+            this.setAnonIdFromLocalStorage();
+        }
+        // Make sure if there is a value, to store it in the local storage
+        if (this.globalModule.userId != null) {
+            localStorage.setItem('anonId', this.globalModule.userId);
+        }
+    }
+
+    private setAnonIdFromQueryParams(queryParams: Dictionary<string | (string | null)[]>): boolean {
+        let anonIdValues = queryParams["anonId"];
+        if (anonIdValues == null) {
+            return false;
+        }
+        let anonId: string | null;
+        if (typeof anonIdValues === 'string') {
+            anonId = anonIdValues;
+        } else {
+            anonId = anonIdValues[0];
+        }
+        if (anonId == null) {
+            return false;
+        }
+        this.globalModule.setUserId(anonId);
+        return true;
+    }
+
+    private setAnonIdFromLocalStorage(): boolean {
+        var anonId = localStorage.getItem('anonId');
+        if (anonId == null) {
+            return false;
+        }
+        this.globalModule.setUserId(anonId);
+        return true;
+    }
 }
 </script>
